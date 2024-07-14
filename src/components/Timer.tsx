79a6d65c75bba49e9208f1bbc00b155643cd2269
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-15 01:31:39 +0900

    feat: Modify animation in Timer
    Timer 시계의 animation을 timerVariants 의 prop의 값 수정을 통해 변경함

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -66,16 +66,16 @@ export default function Timer() {
 
   const timerVariants = {
     initial: {
-      opacity: 0,
-      scale: 0,
+      opacity: 0.5,
+      scale: 0.5,
     },
     animate: {
       opacity: 1,
       scale: 1,
     },
     exit: {
-      opacity: 0,
-      scale: 0,
+      opacity: 0.5,
+      scale: 0.5,
     },
   };
 
