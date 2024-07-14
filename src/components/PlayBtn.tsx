d40f60588ac56d3e746da293bd8ed6c7739f5da1
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-14 23:36:36 +0900

    feat: Add PlayIcon, PauseIcon svg in PlayBtn
    재생과 멈춤 상태 svg 아이콘을 PlayBtn 버튼 컴포넌트에 추가

--- src/components/PlayBtn.tsx
+++ src/components/PlayBtn.tsx
@@ -3,12 +3,40 @@ import { motion } from "framer-motion";
 import { useRecoilState } from "recoil";
 import { isPlayingState } from "../utils/atoms";
 
+export function PauseIcon() {
+  return (
+    <svg
+      data-slot="icon"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path d="M5.75 3a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 0 0 .75-.75V3.75A.75.75 0 0 0 7.25 3h-1.5ZM12.75 3a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 0 0 .75-.75V3.75a.75.75 0 0 0-.75-.75h-1.5Z"></path>
+    </svg>
+  );
+}
+
+export function PlayIcon() {
+  return (
+    <svg
+      data-slot="icon"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path d="M6.3 2.84A1.5 1.5 0 0 0 4 4.11v11.78a1.5 1.5 0 0 0 2.3 1.27l9.344-5.891a1.5 1.5 0 0 0 0-2.538L6.3 2.841Z"></path>
+    </svg>
+  );
+}
+
 export default function PlayBtn() {
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
   return (
     <button onClick={() => setIsPlaying(!isPlaying)}>
-      {isPlaying ? "PAUSE" : "PLAY"}
+      {isPlaying ? <PauseIcon /> : <PlayIcon />}
     </button>
   );
 }
