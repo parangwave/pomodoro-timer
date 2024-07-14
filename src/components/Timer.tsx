5bba36f25f438299a32cea2250a6aa242800ab03
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-14 22:58:38 +0900

    feat: Seperate to Timer Component
    타이머 시계 부분의 코드를 Timer 컴포넌트로 분리함

---
+++ src/components/Timer.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  timerState,
+  isPlayingState,
+  roundsState,
+  goalsState,
+  MAX_MINUTES,
+} from "../utils/atoms";
+
+export default function Timer() {
+  const [time, setTime] = useRecoilState(timerState);
+  const isPlaying = useRecoilValue(isPlayingState);
+  const [rounds, setRounds] = useRecoilState(roundsState);
+  const [goals, setGoals] = useRecoilState(goalsState);
+
+  useEffect(() => {
+    // 재생중 && 시간 > 0
+    if (isPlaying && time > 0) {
+      const interval = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+    // 시간 === 0 (재생 멈춤 x)
+    else if (time === 0) {
+      if (rounds < 3) {
+        setRounds(rounds + 1);
+      } else {
+        setGoals(goals + 1);
+        setRounds(0);
+      }
+      setTime(MAX_MINUTES); // reset to 25 minutes
+    }
+  }, [isPlaying, time, rounds, goals, setTime, setRounds, setGoals]);
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  return (
+    <div>
+      <h1>{formatTime(time)}</h1>
+    </div>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,53 +1,9 @@
 import { useEffect, useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import {
-  timerState,
-  isPlayingState,
-  roundsState,
-  goalsState,
-  MAX_MINUTES,
-} from "../utils/atoms";
 
 export default function App() {
-  const [time, setTime] = useRecoilState(timerState);
-  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
-  const [rounds, setRounds] = useRecoilState(roundsState);
-  const [goals, setGoals] = useRecoilState(goalsState);
-
-  useEffect(() => {
-    // 재생중 && 시간 > 0
-    if (isPlaying && time > 0) {
-      const interval = setInterval(() => {
-        setTime(time - 1);
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-    // 시간 === 0 (재생 멈춤 x)
-    else if (time === 0) {
-      if (rounds < 3) {
-        setRounds(rounds + 1);
-      } else {
-        setGoals(goals + 1);
-        setRounds(0);
-      }
-      setTime(MAX_MINUTES); // reset to 25 minutes
-    }
-  }, [isPlaying, time, rounds, goals, setTime, setRounds, setGoals]);
-
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
-  };
-
   return (
     <>
       <h1>Pomodoro</h1>
-      <div>
-        <h1>{formatTime(time)}</h1>
-      </div>
       <button onClick={() => setIsPlaying(!isPlaying)}>
         {isPlaying ? "PAUSE" : "PLAY"}
       </button>
