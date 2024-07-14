96edc16cb86e476e5075326b9914b54c9b4dd40a
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-15 01:02:31 +0900

    feat: Import Timer in App
    Timer 컴포넌트를 App에 import

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
+import Timer from "../components/Timer";
 
 export default function App() {
   return (
     <>
-      <h1>Pomodoro</h1>
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? "PAUSE" : "PLAY"}
-      </button>
-      <section>
-        <article>
-          <span>{rounds}/4</span>
-          <h5>ROUND</h5>
-        </article>
-        <article>
-          <span>{goals}/12</span>
-          <h5>GOAL</h5>
-        </article>
-      </section>
+      <Timer />
     </>
   );
 }
